1a42495dcc4408c75cfe902dc9e772d875be9283
Sagar mundinamani <160645907+Sagarmundinamani@users.noreply.github.com>
2025-03-05 15:52:24 +0530

    Create GemStones.java
    Question:In a mystical land, a wise sage collects gemstones. Each time he collects a new gemstone, he wonders how many of them are more radiant than any he has collected before Can you help the sage count these radiant gems?
    
    Explanation:
    
        1.Initialize maxRadiance to the smallest possible integer (Integer.MIN_VALUE).
        2.Iterate through the gems array:
         3.If the current gem's radiance is greater than maxRadiance, it is counted as "radiant".
           4. Update maxRadiance to this new value.
        Return the count of radiant gemstones.

---
+++ GemStones.java
@@ -0,0 +1,27 @@
+import java.util.*;
+    public class Arrays {
+        public static void main (String args[]) {
+            Scanner sc = new Scanner((System.in));
+            System.out.println("enter the number of the stones:");
+            int n = sc.nextInt();
+            int gems[] = new int[n];
+            System.out.println("enter the radiance value of the gems:");
+            for (int i = 0; i < n; i++) {
+                gems[i] = sc.nextInt();
+            }
+            int result = countRadianceGems(gems);
+            System.out.println("number of the more radiance gem" + result);
+        }
+        public static int countRadianceGems(int gems[]){
+            int maxRadiance=Integer.MIN_VALUE;
+            int count=0;
+            for(int gem:gems){
+                if(gem> maxRadiance){
+                    maxRadiance=gem;
+                    count++;
+                }
+
+            }
+            return count;
+        }
+    }
