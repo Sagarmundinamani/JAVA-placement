fb55821facf3deed454b3d9d02fc261ba69b1090
Sagar mundinamani <160645907+Sagarmundinamani@users.noreply.github.com>
2025-03-05 12:23:04 +0530

    Update CountSundays.java

--- CountSundays.java
+++ CountSundays.java
@@ -1,28 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class CountSunday {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+public class CountSundays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-            // Input: Start day and number of days in the month
-            System.out.println("Enter the starting day of the month (Monday = 1, Sunday = 7):");
-            int startDay = sc.nextInt();
-            System.out.println("Enter the total number of days in the month:");
-            int totalDays = sc.nextInt();
+        // Input: Start day and number of days in the month
+        System.out.println("Enter the starting day of the month (Monday = 0, Sunday = 6):");
+        int startDay = sc.nextInt();
+        System.out.println("Enter the total number of days in the month:");
+        int totalDays = sc.nextInt();
+        
+        sc.close();
 
-            sc.close();
+        int sundayCount = 0;
 
-            int sundayCount = 0;
-
-
-            // Checking for Sundays, starting from day = 1
-            for (int day = 1; day <= totalDays; day++) {
-                if ((startDay + day - 1) % 7 == 0) { 
-                    sundayCount++;
-                }
+        // Checking for Sundays
+        for (int day = 0; day < totalDays; day++) {
+            if ((startDay + day) % 7 == 6) { // 6 represents Sunday
+                sundayCount++;
             }
-
-            // Output result
-            System.out.println("Number of Sundays in the month: " + sundayCount);
         }
+
+        // Output result
+        System.out.println("Number of Sundays in the month: " + sundayCount);
     }
+}
